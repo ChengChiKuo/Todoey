53f31817ec6f15817a5d5134f034593220137db7
Jesse Kuo <jesse.kuo@gmail.com>
2018-02-01 14:07:57 +0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by CHENG CHI KUO on 2018/2/1.
+//  Copyright © 2018年 CHENG CHI KUO. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
